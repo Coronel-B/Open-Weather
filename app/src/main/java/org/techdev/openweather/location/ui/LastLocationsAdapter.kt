63ea9848ee9864feb20cb60e9f293b7ad534db96
Braian <braiancoronelosmar@gmail.com>
2020-06-10 03:15:46 -0300

    Adapter in dev

--- app/src/main/java/org/techdev/openweather/location/ui/LastLocationsAdapter.kt
+++ app/src/main/java/org/techdev/openweather/location/ui/LastLocationsAdapter.kt
@@ -1,10 +1,14 @@
 package org.techdev.openweather.location.ui
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import org.techdev.openweather.R
 import org.techdev.openweather.location.domain.LastLocation
 
 class LastLocationsAdapter :
@@ -12,15 +16,28 @@ class LastLocationsAdapter :
 
         class LastLocationViewHolder(lastLocationView: View) : RecyclerView.ViewHolder(lastLocationView) {
 
+                private val name = lastLocationView.findViewById<TextView>(R.id.name)
+                private val delete = lastLocationView.findViewById<Button>(R.id.delete)
+
+                fun bind(lastLocation: LastLocation) {
+                        name.text = lastLocation.name
+
+//                        TODO: Delegar escucha del boton en el adapter.
+
+                }
+
         }
 
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LastLocationViewHolder {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LastLocationViewHolder =
+                LastLocationViewHolder(
+                        LayoutInflater.from(parent.context).inflate(
+                                R.layout.list_item_last_location, parent, false
+                        )
+                )
 
         override fun onBindViewHolder(holder: LastLocationViewHolder, position: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                holder.bind(getItem(position))
         }
 
 }
